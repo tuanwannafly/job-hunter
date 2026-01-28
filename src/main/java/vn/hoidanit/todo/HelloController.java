04ecdc81bfa6523211d0cabd8ccb85d2bae40908
testbest <tunawillgetit1030@gmail.com>
2026-01-28 18:57:05 +0700

    chapter 5

--- src/main/java/vn/hoidanit/todo/HelloController.java
+++ src/main/java/vn/hoidanit/todo/HelloController.java
@@ -1,7 +1,10 @@
 package vn.hoidanit.todo;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.todo.Entity.Todo;
 
 @RestController
 public class HelloController {
@@ -11,7 +14,14 @@ public class HelloController {
      * @return the plain-text greeting "Hello World from Spring Boot with tuan"
      */
     @GetMapping("/")
-    public String index() {
-        return "Hello World from Spring Boot with tuan";
+    public ResponseEntity<String> index() {
+        // return "Hello World from Spring Boot with tuan";
+        return ResponseEntity.ok().body("Hello World ");
+    }
+
+    @GetMapping("/tuanit")
+    public ResponseEntity<Todo> indexx() {
+        Todo test = new Todo("tuan", true);
+        return ResponseEntity.ok().body(test);
     }
 }
